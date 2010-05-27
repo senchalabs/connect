481b63209ce22e483b91320efd59d0a3c4b9c529
visionmedia <tj@vision-media.ca>
2010-05-26 19:53:28 -0700

    Added 404 provider tests

---
+++ test/connect.providers.404.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var connect = require('connect'),
+    helpers = require('./helpers'),
+    assert = require('assert'),
+    http = require('http');
+
+Ext.test('Connect 404', {
+    test: function(){
+        var server = connect.run([]);
+        var req = server.request('POST', '/', { buffer: true });
+        req.addListener('response', function(res){
+            res.addListener('end', function(){
+                assert.strictEqual(404, res.statusCode, 'Test 404 provider status code');
+                assert.equal('Not Found', res.body, 'Test 404 response body');
+                assert.equal('text/plain', res.headers['content-type'], 'Test 404 Content-Type');
+            })
+        })
+        req.end();
+    }
+})
