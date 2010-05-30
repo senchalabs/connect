58fc28c42b97b9d43da176aad7974e54f864d70f
visionmedia <tj@vision-media.ca>
2010-05-30 15:27:14 -0700

    Started debug panel middleware

--- examples/error/app.js
+++ examples/error/app.js
@@ -1,9 +1,10 @@
 
 require('./lib/connect').createServer([
+    { filter: 'debug' },
     { module: {
         handle: function(err, req, res, next){
             throw new Error('fail');
         }
     }},
-    { filter: 'error-handler', param: { dumpExceptions: true, showStack: true }}
+    { filter: 'error-handler', param: { showStack: true }}
 ]).listen();
