813e13d7a998965d2fac102a12793e986439bbc4
visionmedia <tj@vision-media.ca>
2010-05-31 11:04:02 -0700

    Implemented simple json-rpc calls

--- test/connect.providers.rpc.test.js
+++ test/connect.providers.rpc.test.js
@@ -39,8 +39,8 @@ module.exports = {
             params: [1,2],
             id: 1
         }, function(res, body){
-            assert.eql({ id: null, error: { code: jsonrpc.INVALID_REQUEST, message: 'Invalid Request.' }, jsonrpc: '2.0'}, body);
-        })
+            assert.eql({ id: 1, error: { code: jsonrpc.INVALID_REQUEST, message: 'Invalid Request.' }, jsonrpc: '2.0'}, body);
+        });
     },
     
     test_invalid_id: function(){
@@ -50,14 +50,41 @@ module.exports = {
             method: 'add',
             params: [1,2]
         }, function(res, body){
-            assert.eql({ id: null, error: { code: jsonrpc.INVALID_REQUEST, message: 'Invalid Request.' }, jsonrpc: '2.0'}, body);
-        })
+            assert.eql({ id: null, error: { code: jsonrpc.INVALID_REQUEST, message: 'Invalid Request.' }, jsonrpc: '2.0' }, body);
+        });
     },
     
     test_parse_error: function(){
         var server = run({});
         server.call('{ "invalid:', function(res, body){
-            assert.eql({ id: null, error: { code: jsonrpc.PARSE_ERROR, message: 'Parse Error.' }, jsonrpc: '2.0'}, body);
-        })
+            assert.eql({ id: null, error: { code: jsonrpc.PARSE_ERROR, message: 'Parse Error.' }, jsonrpc: '2.0' }, body);
+        });
+    },
+    
+    test_invalid_method: function(){
+        var server= run({});
+        server.call({
+            jsonrpc: '2.0',
+            method: 'add',
+            id: 1
+        }, function(res, body){
+            assert.eql({ id: 1, error: { code: jsonrpc.METHOD_NOT_FOUND, message: 'Method Not Found.' }, jsonrpc: '2.0' }, body);
+        });
+    },
+    
+    test_method_call: function(){
+        var server= run({
+            add: function(a, b, fn){
+                fn(null, a + b);
+            }
+        });
+        server.call({
+            jsonrpc: '2.0',
+            method: 'add',
+            params: [1,2],
+            id: 1
+        }, function(res, body){
+            assert.eql({ id: 1, result: 3, jsonrpc: '2.0' }, body);
+        });
     }
 }
