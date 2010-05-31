aa53899d09efb6a8bc984a69aa0d9350185d9b55
visionmedia <tj@vision-media.ca>
2010-05-31 13:21:58 -0700

    Prevent catching of exceptions thrown in services
    You must handle this your-self if you wish

--- test/connect.providers.rpc.test.js
+++ test/connect.providers.rpc.test.js
@@ -72,22 +72,6 @@ module.exports = {
         });
     },
     
-    test_uncaught_method_exception: function(){
-        var server = run({
-            add: function(a, b){
-                throw new Error('fail!');
-            }
-        });
-        server.call({
-            jsonrpc: '2.0',
-            method: 'add',
-            params: [1,2],
-            id: 1
-        }, function(res, body){
-            assert.eql({ id: 1, error: { code: jsonrpc.INTERNAL_ERROR, message: 'Internal Error.' }, jsonrpc: '2.0' }, body);
-        });
-    },
-    
     test_passing_method_exceptions: function(){
         var server = run({
             add: function(a, b){
