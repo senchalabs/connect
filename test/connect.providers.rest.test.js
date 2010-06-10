ea426a7d5bb5bd87f30cecae505907eb8119427f
visionmedia <tj@vision-media.ca>
2010-06-10 07:16:40 -0700

    Implemented most of the RESTful routing

--- test/connect.providers.rest.test.js
+++ test/connect.providers.rest.test.js
@@ -22,10 +22,22 @@ module.exports = {
                     '/': function(req, res){
                         res.writeHead(200, {});
                         res.end('got /');
+                    },
+                    '/user/:id': function(req, res, params){
+                        res.writeHead(200, {});
+                        res.end('got user ' + params.id);
+                    },
+                    '/user/:id/:operation': function(req, res, params){
+                        res.writeHead(200, {});
+                        res.end(params.operation + 'ing user ' + params.id);
                     }
                 }
             }}
         ]);
         server.assertResponse('GET', '/', 200, 'got /', 'Test rest GET /');
+        server.assertResponse('GET', '/user', 404, 'Cannot find /user', 'Test rest GET unmatched path param');
+        server.assertResponse('GET', '/user/12', 200, 'got user 12', 'Test rest GET matched path param');
+        server.assertResponse('GET', '/user/12/', 200, 'got user 12', 'Test rest GET matched path param with trailing slash');
+        server.assertResponse('GET', '/user/99/edit', 200, 'editing user 99', 'Test rest GET matched path with several params');
     }
 }
