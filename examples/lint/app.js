de868e4c2ede7f1efc8685b1258b0252e3e245df
visionmedia <tj@vision-media.ca>
2010-06-11 15:40:22 -0700

    Warning about req header capitals

--- examples/lint/app.js
+++ examples/lint/app.js
@@ -1,6 +1,5 @@
 
 module.exports = require('./../../lib/connect').createServer([
-    { filter: 'lint' },
     { module: {
         // No named params
         handle: function(){
@@ -10,7 +9,22 @@ module.exports = require('./../../lib/connect').createServer([
     { module: {
         // Does not call next AND does not respond
         handle: function(req, res, next){
+            // Call foo() instead so
+            // that our demo can still function.
+            var foo = next;
+            foo();
             
+            // All good
+            var ct = req.headers['content-type'];
         }
-    }}
+    }},
+    { module: {
+        handle: function(req, res, next){
+            // Request headers are always normalized as
+            // lowercased by ryan's http parser.
+            var ct = req.headers['Content-Type'];
+            next();
+        }
+    }},
+    { filter: 'lint' }
 ]);
