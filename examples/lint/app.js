0750bfe46f8703e3313b8511493e0fe4fb44d6d4
visionmedia <tj@vision-media.ca>
2010-06-11 15:29:53 -0700

    lint warnings when handle() does not seem to respond or call next()

--- examples/lint/app.js
+++ examples/lint/app.js
@@ -2,13 +2,15 @@
 module.exports = require('./../../lib/connect').createServer([
     { filter: 'lint' },
     { module: {
+        // No named params
         handle: function(){
-            // No named params
+            arguments[2]();
         }
     }},
     { module: {
+        // Does not call next AND does not respond
         handle: function(req, res, next){
-            // Does not call next() AND does not respond
+            
         }
     }}
 ]);
