38954866ecb87a316061e27ded3961f445dff902
visionmedia <tj@vision-media.ca>
2010-06-11 10:46:10 -0700

    Started sample rest app

--- examples/rest/app.js
+++ examples/rest/app.js
@@ -6,10 +6,33 @@ var users = [
 
 var userRoutes = {
     get: {
-        '/.format?': function(req, res, params){
+        '.format?': function(req, res, params){
             var body = '<ul>' 
                 + users.map(function(user){ return '<li>' + user.name + '</li>'; }).join('\n')
                 + '</ul>';
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': body.length
+            });
+            res.end(body, 'utf8');
+        },
+        '/:id': function(req, res, params){
+            var body = users[params.id]
+                ? users[params.id].name
+                : 'User ' + params.id + ' does not exist';
+            res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': body.length
+            });
+            res.end(body, 'utf8');
+        }
+    }
+};
+
+var mainRoutes = {
+    get: {
+        '/': function(){
+            var body = 'Visit /users, /users/0, or /users.json';
             res.writeHead(200, {
                 'Content-Type': 'text/html',
                 'Content-Length': body.length
