303fec06210d9926200b7aba5028e77761449aec
visionmedia <tj@vision-media.ca>
2010-06-11 13:58:21 -0700

    jsonrpc no longer needs to be passed "param"

--- examples/jsonrpc/app.js
+++ examples/jsonrpc/app.js
@@ -1,8 +1,8 @@
 
-// curl -H Content-Type: application/json -d { "jsonrpc": "2.0", "method": "add", "params": [1,2], "id":2 } http://localhost:3000
+// curl -H "Content-Type: application/json" -d "{ "jsonrpc": "2.0", "method": "add", "params": [1,2], "id":2 }" http://localhost:3000
 
 module.exports = require('./lib/connect').createServer([
-    { provider: 'jsonrpc', param: {
+    { provider: 'jsonrpc', methods: {
         add: function(a, b){
             try {
                 this(null, a + b);
