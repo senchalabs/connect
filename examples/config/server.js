2327aa4c528c9ef7420c2bf1f21c466fd279c5c1
Tj Holowaychuk <tj@vision-media.ca>
2010-06-14 09:05:22 -0700

    Fixed config example. Closes #15
    Should be good to go now.

--- examples/config/server.js
+++ examples/config/server.js
@@ -1,9 +1,11 @@
 
-var connect = require('./../../lib/connect');
+var sys = require('sys'),
+	connect = require('./../../lib/connect');
 
 // Based on the current env name require / mixin the 
 // additional file-based configuration
 process.connectEnv.mixin(require('./config/' + process.connectEnv.name));
+sys.log('loading config file "config/' + process.connectEnv.name + '.js"');
 
 // Pass the custom environment as the second argument to createServer()
 module.exports = connect.createServer([
@@ -11,7 +13,7 @@ module.exports = connect.createServer([
     { module: {
         handle: function(req, res, next){
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(JSON.stringify(process.connectEnv));
+            res.end(sys.inspect(process.connectEnv));
         }
     }}
 ]);
