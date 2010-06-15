fbb40f7e79a6eada9a6af974a96e17795348cf3e
Tj Holowaychuk <tj@vision-media.ca>
2010-06-15 07:25:27 -0700

    Added utils.base64{Encode,Decode}()

--- examples/session/app.js
+++ examples/session/app.js
@@ -6,11 +6,6 @@
 var sys = require('sys'),
     MemoryStore = require('./../../lib/connect/filters/session/memory').MemoryStore;
 
-// TODO: re-generation
-// TODO: clearing
-// TODO: session clearing
-// TODO: length
-
 module.exports = require('./../../lib/connect').createServer([
     { filter: 'body-decoder' },
     { filter: 'cookie' },
@@ -20,12 +15,14 @@ module.exports = require('./../../lib/connect').createServer([
             if (req.method === 'POST') {
                 switch (req.body.op) {
                     case 'Update':
+                        // Update session information
                         for (var key in req.body) {
                             req.session[key] = req.body[key];
                         }
                         sys.puts('Updated session');
                         break;
                     case 'Reset':
+                        // Reset the session by assigning a new object
                         req.session = {};
                         sys.puts('Reset sesssion');
                         break;
@@ -38,6 +35,7 @@ module.exports = require('./../../lib/connect').createServer([
         handle: function(req, res, next){
             sys.puts(sys.inspect(req.session));
             res.writeHead(200, { 'Content-Type': 'text/html' });
+            // Fetch session length and display form
             req.sessionStore.length(function(err, n){
                 res.end('<form method="post">'
                     + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
