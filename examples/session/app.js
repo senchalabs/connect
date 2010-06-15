1373a95174cf18121d35ac00cc0842a4acf15314
Tj Holowaychuk <tj@vision-media.ca>
2010-06-15 07:00:00 -0700

    Added MemoryStore#length()

--- examples/session/app.js
+++ examples/session/app.js
@@ -12,18 +12,32 @@ var sys = require('sys'),
 // TODO: length
 
 module.exports = require('./../../lib/connect').createServer([
+    { filter: 'body-decoder' },
     { filter: 'cookie' },
-    { filter: 'session', store: new MemoryStore },
+    { filter: 'session' },
     { module: {
         handle: function(req, res, next){
-            if (!req.session.name) {
-                req.session.name = 'tj';
-                sys.puts('set name');
-            } else {
-                sys.puts('got name ' + req.session.name);
+            if (req.method === 'POST') {
+                for (var key in req.body) {
+                    req.session[key] = req.body[key];
+                }
+                sys.puts('Updated name to ' + req.session.name);
             }
-            res.writeHead(200, {});
-            res.end('Wahoo');
+            next();
+        }
+    }},
+    { module: {
+        handle: function(req, res, next){
+            sys.puts(sys.inspect(req.session));
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            req.sessionStore.length(function(err, n){
+                res.end('<form method="post">'
+                    + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
+                    + 'Email: <input type="text" name="email" value="' + (req.session.email || '') + '"/>'
+                    + '<input type="submit" value="Change"/>' 
+                    + '<p>Online: ' + n + '</p>' 
+                    + '</form>');
+            });
         }
     }}
 ]);
