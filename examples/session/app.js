6f5ee70e8898a6e333115acbfee418ac3631329e
Tj Holowaychuk <tj@vision-media.ca>
2010-06-14 20:55:28 -0700

    Todo

--- examples/session/app.js
+++ examples/session/app.js
@@ -6,6 +6,11 @@
 var sys = require('sys'),
     MemoryStore = require('./../../lib/connect/filters/session/memory').MemoryStore;
 
+// TODO: re-generation
+// TODO: clearing
+// TODO: session clearing
+// TODO: length
+
 module.exports = require('./../../lib/connect').createServer([
     { filter: 'cookie' },
     { filter: 'session', store: new MemoryStore },
