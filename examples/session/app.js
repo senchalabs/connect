44238f34bf4608ea42338bd65f2deeec57ab859f
Tj Holowaychuk <tj@vision-media.ca>
2010-06-15 15:31:35 -0700

    Session example using flash

--- examples/session/app.js
+++ examples/session/app.js
@@ -6,45 +6,52 @@
 var sys = require('sys'),
     MemoryStore = require('./../../lib/connect/filters/session/memory').MemoryStore;
 
+// One minute
+
+var minute = 60000;
+
 module.exports = require('./../../lib/connect').createServer([
+    { filter: 'log' },
     { filter: 'body-decoder' },
     { filter: 'cookie' },
-    { filter: 'session', store: new MemoryStore({ reapInterval: 5000, maxAge: 25000 }) },
-    { module: {
-        handle: function(req, res, next){
-            if (req.method === 'POST') {
+    { filter: 'session', store: new MemoryStore({ reapInterval: minute, maxAge: minute * 5 }) },
+    { filter: 'flash' },
+    { provider: 'rest', routes: {
+        get: {
+            '/': function(req, res){
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                // Fetch number of "online" users
+                req.sessionStore.length(function(err, n){
+                    req.flash('info').forEach(function(msg){
+                        res.write('<p>' + msg + '</p>');
+                    });
+                    
+                    if (req.session.name) {
+                        res.write('<p>Welcome ' + req.session.name + '</p>');
+                    } else {
+                        res.write('<form method="post">'
+                            + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
+                            + '<input type="submit" value="Join" name="op" />' 
+                            + '</form>');
+                    }
+                    if (n !== undefined) {
+                        res.write('<p>Online: ' + n + '</p>');
+                    }
+                    res.end();
+                });
+            }
+        },
+        post: {
+            '/': function(req, res){
                 switch (req.body.op) {
-                    case 'Update':
-                        // Update session information
-                        for (var key in req.body) {
-                            req.session[key] = req.body[key];
-                        }
-                        sys.puts('Updated session');
-                        break;
-                    case 'Reset':
-                        // Reset the session by assigning a new object
-                        req.session = {};
-                        sys.puts('Reset sesssion');
+                    case 'Join':
+                        var name = req.session.name = req.body.name;
+                        req.message('info', 'joined as "' + name + '"');
+                        res.writeHead(303, { 'Location': '/' });
+                        res.end();
                         break;
                 }
             }
-            next();
-        }
-    }},
-    { module: {
-        handle: function(req, res, next){
-            sys.puts(sys.inspect(req.session));
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            // Fetch session length and display form
-            req.sessionStore.length(function(err, n){
-                res.end('<form method="post">'
-                    + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
-                    + 'Email: <input type="text" name="email" value="' + (req.session.email || '') + '"/>'
-                    + '<input type="submit" value="Update" name="op" />' 
-                    + '<input type="submit" value="Reset" name="op" />' 
-                    + (n === undefined ? '' : '<p>Online: ' + n + '</p>')
-                    + '</form>');
-            });
         }
     }}
 ]);
