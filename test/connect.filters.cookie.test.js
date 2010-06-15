4f72cb9062364eaad0f6b30ebc7a6408f5485426
Tj Holowaychuk <tj@vision-media.ca>
2010-06-15 09:15:29 -0700

    Added tests for cookie middelware

--- test/connect.filters.cookie.test.js
+++ test/connect.filters.cookie.test.js
@@ -10,9 +10,24 @@ var connect = require('connect'),
 
 module.exports = {
     test: function(){
+        var n = 0;
         var server = helpers.run([
             { filter: 'cookie' },
-            { module: require('./providers/echo') }
+            { module: {
+                handle: function(req, res, next){
+                    switch (n++) {
+                        case 0:
+                            assert.eql({}, req.cookies, 'Test cookies default to blank object');
+                            break;
+                        case 1:
+                            assert.eql({ sid: '123' }, req.cookies, 'Test cookie parsing');
+                            break;
+                    }
+                    next();
+                }
+            }}
         ]);
+        server.request('GET', '/').end();
+        server.request('GET', '/', { 'Cookie': 'sid=123' }).end();
     }
 }
