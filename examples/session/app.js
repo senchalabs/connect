105e1145c43aeee5cfc8faaeb1c4762dc591abd3
Tj Holowaychuk <tj@vision-media.ca>
2010-06-14 17:19:00 -0700

    Basic cookie session support working

--- examples/session/app.js
+++ examples/session/app.js
@@ -1,9 +1,21 @@
 
+/**
+ * Module dependencies.
+ */
+
+var sys = require('sys');
+
 module.exports = require('./../../lib/connect').createServer([
     { filter: 'cookie' },
     { filter: 'session' },
     { module: {
         handle: function(req, res, next){
+            if (!req.session.name) {
+                req.session.name = 'tj';
+                sys.puts('set name');
+            } else {
+                sys.puts('got name ' + req.session.name);
+            }
             res.writeHead(200, {});
             res.end('Wahoo');
         }
