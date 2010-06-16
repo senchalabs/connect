adf5a5facec2376974bff12eacab6aeb8fab7ec5
Tj Holowaychuk <tj@vision-media.ca>
2010-06-16 08:44:08 -0700

    Added more session tests

--- test/connect.filters.session.test.js
+++ test/connect.filters.session.test.js
@@ -39,24 +39,28 @@ module.exports = {
                             assert.equal(sid, req.session.id, 'Test MemoryStore persistence');
                             assert.notEqual(lastAccess, req.session.lastAccess, 'Test MemoryStore lastAccess update');
                             break;
+                        case 4:
+                            assert.notEqual(sid, req.session.id, 'Test MemoryStore User-Agent fingerprint');
+                            break;
                     }
                     next();
                 }
             }}
         ]);
         
-        server.pending = 4;
+        server.pending = 5;
         server.request('GET', '/').end();
         server.request('GET', '/', { 'Cookie': 'connect.sid=123123' }).end();
         
-        var req = server.request('GET', '/');
+        var req = server.request('GET', '/', { 'User-Agent': 'foo' });
         req.addListener('response', function(res){
             var setCookie = res.headers['set-cookie'];
             sid = setCookie.match(/connect\.sid=([^;]+)/)[1];
             assert.ok(setCookie.indexOf('connect.sid=') === 0, 'Test MemoryStore Set-Cookie connect.sid');
             assert.ok(setCookie.indexOf('httpOnly') !== -1, 'Test MemoryStore Set-Cookie httpOnly');
             assert.ok(setCookie.indexOf('expires=') !== -1, 'Test MemoryStore Set-Cookie expires');
-            server.request('GET', '/', { 'Cookie': 'connect.sid=' + sid }).end()
+            server.request('GET', '/', { 'Cookie': 'connect.sid=' + sid, 'User-Agent': 'foo' }).end();
+            server.request('GET', '/', { 'Cookie': 'connect.sid=' + sid, 'User-Agent': 'bar' }).end();
         });
         req.end();
     },
