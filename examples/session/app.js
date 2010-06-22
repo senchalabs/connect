f8ac231115b9ef975322dec6b478117083f74d24
Tj Holowaychuk <tj@vision-media.ca>
2010-06-22 07:32:43 -0700

    Fixed session example
    Changes due to rest api changes

--- examples/session/app.js
+++ examples/session/app.js
@@ -17,53 +17,48 @@ module.exports = require('./../../lib/connect').createServer([
     { filter: 'redirect' },
     { filter: 'session', store: new MemoryStore({ reapInterval: minute, maxAge: minute * 5 }) },
     { filter: 'flash' },
-    { provider: 'rest', routes: {
-        get: {
-            '/': function(req, res){
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                // Fetch number of "online" users
-                req.sessionStore.length(function(err, n){
-                    
-                    // Display flash messages
-                    req.flash('info').forEach(function(msg){
-                        res.write('<p>' + msg + '</p>');
-                    });
-                    
-                    // User joined
-                    if (req.session.name) {
-                        res.write('<p>Welcome ' + req.session.name + '</p>');
-                    // User has not "joined", display the form
-                    } else {
-                        res.write('<form method="post">'
-                            + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
-                            + '<input type="submit" value="Join" name="op" />' 
-                            + '</form>');
-                    }
-                    
-                    // Display online count
-                    res.write('<p>Online: ' + n + '</p>');
-                    res.end();
+    { provider: 'rest', app: function(app) {
+        app.get('/', function(req, res){
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            // Fetch number of "online" users
+            req.sessionStore.length(function(err, n){
+                // Display flash messages
+                req.flash('info').forEach(function(msg){
+                    res.write('<p>' + msg + '</p>');
                 });
-            },
-            '/logout': function(req, res){
-                req.sessionStore.regenerate(req, function(err){
-                    req.flash('info', 'Logged out');
-                    req.redirect('/');
-                });
-            }
-        },
-        post: {
-            '/': function(req, res){
-                switch (req.body.op) {
-                    case 'Join':
-                        req.sessionStore.regenerate(req, function(err){
-                            var name = req.session.name = req.body.name;
-                            req.flash('info', 'joined as _"' + name + '"_ click [here](/logout) to logout.');
-                            req.redirect('/'); 
-                        });
-                        break;
+    
+                // User joined
+                if (req.session.name) {
+                    res.write('<p>Welcome ' + req.session.name + '</p>');
+                // User has not "joined", display the form
+                } else {
+                    res.write('<form method="post">'
+                        + 'Name: <input type="text" name="name" value="' + (req.session.name || '') + '"/>'
+                        + '<input type="submit" value="Join" name="op" />' 
+                        + '</form>');
                 }
+    
+                // Display online count
+                res.write('<p>Online: ' + n + '</p>');
+                res.end();
+            });
+        });
+        app.get('/logout', function(req, res){
+            req.sessionStore.regenerate(req, function(err){
+                req.flash('info', 'Logged out');
+                res.redirect('/');
+            });
+        });
+        app.post('/', function(req, res){
+            switch (req.body.op) {
+                case 'Join':
+                    req.sessionStore.regenerate(req, function(err){
+                        var name = req.session.name = req.body.name;
+                        req.flash('info', 'joined as _"' + name + '"_ click [here](/logout) to logout.');
+                        res.redirect('/'); 
+                    });
+                    break;
             }
-        }
+        });
     }}
 ]);
