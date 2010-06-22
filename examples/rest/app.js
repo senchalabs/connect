ec538e68d6d2bec7ae1a6bb9be3aad9a9e553449
Tj Holowaychuk <tj@vision-media.ca>
2010-06-21 18:46:56 -0700

    rest example using new api

--- examples/rest/app.js
+++ examples/rest/app.js
@@ -4,63 +4,60 @@ var users = [
     { name: 'tim' }
 ];
 
-var userRoutes = {
-    get: {
-        '/(all.:format?)?': function(req, res, params){
-            var body;
-            switch (params.format) {
-                case 'json':
-                    body = JSON.stringify(users);
-                    break;
-                default:
-                     body = '<ul>' 
-                        + users.map(function(user){ return '<li>' + user.name + '</li>'; }).join('\n')
-                        + '</ul>';
-            }
-            res.writeHead(200, {
-                'Content-Type': 'text/html',
-                'Content-Length': body.length
-            });
-            res.end(body, 'utf8');
-        },
-        '/:id/:op?': function(req, res, params){
-            var body = users[params.id]
-                ? users[params.id].name
-                : 'User ' + params.id + ' does not exist';
-            body = (params.op || 'view') + 'ing ' + body;
-            res.writeHead(200, {
-                'Content-Type': 'text/html',
-                'Content-Length': body.length
-            });
-            res.end(body, 'utf8');
+function user(app){
+    app.get('/(all.:format?)?', function(req, res, params){
+        var body;
+        switch (params.format) {
+            case 'json':
+                body = JSON.stringify(users);
+                break;
+            default:
+                 body = '<ul>' 
+                    + users.map(function(user){ return '<li>' + user.name + '</li>'; }).join('\n')
+                    + '</ul>';
         }
-    }
-};
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': body.length
+        });
+        res.end(body, 'utf8');
+    });
+    
+    app.get('/:id/:op?', function(req, res, params){
+        var body = users[params.id]
+            ? users[params.id].name
+            : 'User ' + params.id + ' does not exist';
+        body = (params.op || 'view') + 'ing ' + body;
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': body.length
+        });
+        res.end(body, 'utf8');
+    });
+}
 
-var mainRoutes = {
-    get: {
-        '/': function(req, res){
-            var examples = [
-                '/users (or /users/all)',
-                '/users/all.json',
-                '/users/0 (or /users/0/view)',
-                '/users/0/edit'
-            ];
-            var body = 'Visit one of the following: <ul>'
-                + examples.map(function(str){ return '<li>' + str + '</li>' }).join('\n')
-                + '</ul>';
-            res.writeHead(200, {
-                'Content-Type': 'text/html',
-                'Content-Length': body.length
-            });
-            res.end(body, 'utf8');
-        }
-    }
-};
+function main(app){
+    app.get('/', function(req, res, params){
+        var examples = [
+            '/users (or /users/all)',
+            '/users/all.json',
+            '/users/0 (or /users/0/view)',
+            '/users/0/edit'
+        ];
+        var body = 'Visit one of the following: <ul>'
+            + examples.map(function(str){ return '<li>' + str + '</li>' }).join('\n')
+            + '</ul>';
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': body.length
+        });
+        res.end(body, 'utf8');
+    });
+}
 
 module.exports = require('./../../lib/connect').createServer([
     { filter: 'log' },
     { filter: 'repl' },
-    { provider: 'rest', routes: userRoutes, route: '/users' },
-    { provider: 'rest', routes: mainRoutes },
+    { provider: 'rest', app: user, route: '/users' },
+    { provider: 'rest', app: main },
 ]);
