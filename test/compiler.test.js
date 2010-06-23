197cd4dad9e83f5d85d314fc8f928e312b0dbaad
Tj Holowaychuk <tj@vision-media.ca>
2010-06-23 09:05:36 -0700

    Fixed compiler src 404

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -10,11 +10,14 @@ var connect = require('connect'),
 
 module.exports = {
     test: function(){
-        //var server = helpers.run([
-        //    { filter: 'compiler', src: __dirname + '/fixtures', enable: ['sass'] }
-        //]);
-        // server.assertResponse('GET', '/doesnotexist.css', 404, 'Not Found');
-        // server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
-        // server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
+        var server = helpers.run([
+           { filter: 'compiler', src: __dirname + '/fixtures', enable: ['sass'] },
+           { provider: 'static', root: __dirname + '/fixtures' }
+        ]);
+        server.assertResponse('GET', '/doesnotexist.css', 404, 'Not Found');
+        server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
+        server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
+        server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
+        server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
     }
 }
