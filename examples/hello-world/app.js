894cf63623f80830977ad4e334eaf0924a521d1f
Tim Caswell <tim@extjs.com>
2010-06-22 18:26:20 -0700

    Convert more middlewares and simplify the hello-world example.

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -2,7 +2,6 @@ var Connect = require('../../lib/connect');
 
 var Server = module.exports = Connect.createServer(
     Connect.logFilter(),
-    Connect.staticProvider(__dirname),
     function (req, res) {
         res.simpleBody(200, 'Hello World');
     }
