964fe2f95c381a670e586800ce83549baee786b3
Tim Caswell <tim@extjs.com>
2010-06-23 17:38:38 -0700

    Keep the hello-world example small.

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -1,27 +1,10 @@
+// Note that even though this looks like a normal node http app, it's really a
+// valid connect app using the connect middleware stack.
+
 var Connect = require('../../lib/connect');
 
 module.exports = Connect.createServer(
-    Connect.responseTime(),
-    Connect.logger(),
-    Connect.conditionalGet(),
-    Connect.cache(1),
-    Connect.gzip(),
-    Connect.bodyDecoder(),
-    Connect.cookieDecoder(),
-    Connect.debug(),
-    Connect.methodOverride(),
-    Connect.staticProvider(__dirname),
-    Connect.format(),
     function handle(req, res) {
         res.simpleBody(200, {method: req.method, format: req.format, body: req.body});
     }
 );
-
-// Tested:
-"bodyDecoder"
-"cache"
-"conditionalGet"
-
-// Skipped
-"cookie"
-"debug"
