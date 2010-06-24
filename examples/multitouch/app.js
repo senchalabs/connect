19928b03723b40010202649f4d5ac5fdb9967a22
Tim Caswell <tim@extjs.com>
2010-06-23 17:30:52 -0700

    The multitouch demo is now working better than ever.

--- examples/multitouch/app.js
+++ examples/multitouch/app.js
@@ -1,4 +1,9 @@
-require.paths.unshift("./lib");
+// NOTE:
+// When testing this demo you need to ensure that the domain you're hosting
+// it on is in the network section of the cache manifest (second argument)
+// or the long-poll may not work.
+
+require.paths.unshift("../../lib");
 var Connect = require('connect');
 var root = __dirname + "/public";
 
@@ -32,14 +37,25 @@ var Backend = {
 };
 
 
-module.exports = new Connect.Server([
-    {filter: "log"},
-    {filter: "response-time"},
-    {filter: "body-decoder"},
-    {provider: "pubsub", route: "/stream", logic: Backend},
-    {filter: "conditional-get"},
-    {filter: "cache"},
-    {filter: "gzip"},
-    {provider: "cache-manifest", root: root},
-    {provider: "static", root: root}
-]);
+var Server = module.exports = Connect.createServer();
+
+// Add global filters
+Server.use("/",
+    Connect.responseTime(),
+    Connect.logger()
+);
+
+// Serve dynamic responses
+Server.use("/stream",
+    Connect.bodyDecoder(),
+    Connect.pubsub(Backend)
+);
+
+// Serve static resources
+Server.use("/",
+    Connect.cacheManifest(root, ["http://localhost:3000/"]),
+    Connect.conditionalGet(),
+    Connect.cache(),
+    Connect.gzip(),
+    Connect.staticProvider(root)
+);
