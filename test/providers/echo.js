0099c924481681492861a58d825af29c51997468
Tj Holowaychuk <tj@vision-media.ca>
2010-06-24 14:46:21 -0700

    Fixed logger tests

--- test/providers/echo.js
+++ test/providers/echo.js
@@ -1,8 +1,10 @@
 
-exports.handle = function(req, res, next){
-    res.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Conent-Length': req.body.length
-    })
-    res.end(req.body)
-}
+module.exports = function(){
+    return function(req, res, next){
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Conent-Length': req.body.length
+        })
+        res.end(req.body)
+    };
+};
