adae27bd49df52a6ce936d2381b0c1ff2cb3e460
Tj Holowaychuk <tj@vision-media.ca>
2010-06-24 14:41:20 -0700

    Fixed methodOverride tests

--- test/method-override.test.js
+++ test/method-override.test.js
@@ -10,36 +10,47 @@ var connect = require('connect'),
 
 module.exports = {
     'test valid http method': function(){
-        var server = helpers.run([
-            { filter: 'body-decoder' },
-            { filter: 'method-override' },
-            { module: {
-                handle: function(req, res){
-                    assert.equal('PUT', req.method, 'Test method-override')
-                    res.writeHead(200);
-                    res.end();
-                }
-            }}
-        ]);
+        var server = helpers.run(
+            connect.bodyDecoder(),
+            connect.methodOverride(),
+            function(req, res){
+                assert.equal('PUT', req.method, 'Test method-override');
+                res.writeHead(200);
+                res.end();
+            }
+        );
         var req = server.request('POST', '/', { 'Content-Type': 'application/x-www-form-urlencoded' });
         req.write('_method=put');
         req.end();
     },
     
     'test invalid http method': function(){
-        var server = helpers.run([
-            { filter: 'body-decoder' },
-            { filter: 'method-override' },
-            { module: {
-                handle: function(req, res){
-                    assert.equal('POST', req.method, 'Test method-override invalid method')
-                    res.writeHead(200);
-                    res.end();
-                }
-            }}
-        ]);
+        var server = helpers.run(
+            connect.bodyDecoder(),
+            connect.methodOverride(),
+            function(req, res){
+                assert.equal('POST', req.method, 'Test method-override invalid method');
+                res.writeHead(200);
+                res.end();
+            }
+        );
         var req = server.request('POST', '/', { 'Content-Type': 'application/x-www-form-urlencoded' });
         req.write('_method=foobar');
+        req.end();
+    },
+    
+    'test custom key': function(){
+        var server = helpers.run(
+            connect.bodyDecoder(),
+            connect.methodOverride('__method__'),
+            function(req, res){
+                assert.equal('PUT', req.method, 'Test method-override custom key');
+                res.writeHead(200);
+                res.end();
+            }
+        );
+        var req = server.request('POST', '/', { 'Content-Type': 'application/x-www-form-urlencoded' });
+        req.write('__method__=put');
         req.end();
     }
 }
