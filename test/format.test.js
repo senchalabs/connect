c97d5e41c423b7791c0ba9c095de9c94088a2e69
Tj Holowaychuk <tj@vision-media.ca>
2010-06-25 11:01:53 -0700

    Added format tests to confirm route usage

--- test/format.test.js
+++ test/format.test.js
@@ -22,5 +22,16 @@ module.exports = {
         server.assertResponse('GET', '/users/all.xml', 200, 'format "xml" url "/users/all"', 'Test multi-segment path format');
         server.assertResponse('GET', '/users.json?something=json', 200, 'format "json" url "/users?something=json"', 'Test format with querystring');
         server.assertResponse('GET', '/users.json#something', 200, 'format "json" url "/users#something"', 'Test format with fragment');
+    },
+    
+    'test with route': function(){
+        var server = helpers.run(connect.format());
+        server.use('/products', function(req, res){
+            res.writeHead(200, {});
+            res.end('format "' + req.format + '" url "' + req.url + '"');
+        });
+        server.assertResponse('GET', '/products', 200, 'format "undefined" url "/"');
+        server.assertResponse('GET', '/products/.json', 200, 'format "json" url "/"');
+        server.assertResponse('GET', '/products.json', 200, 'format "json" url "/"');
     }
 };
