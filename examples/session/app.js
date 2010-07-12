b97e682982c1f633e615b96ca0085f4747f11952
Tj Holowaychuk <tj@vision-media.ca>
2010-07-12 13:18:31 -0700

    Removed Store#all() call in sess example

--- examples/session/app.js
+++ examples/session/app.js
@@ -44,18 +44,7 @@ function app(app) {
             }
 
             // Display online count
-            res.write('<ul><li>Online: ' + n + '</li>');
-            req.sessionStore.all(function(err, sessions){
-                sessions.forEach(function(sess){
-                    if (sess) {
-                        var d = new Date(sess.lastAccess),
-                            date = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear()
-                                + ' at ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-                        res.write('<li>' + (sess.name || 'guest') + ' was last active on ' + date + '</li>');
-                    }
-                });
-                res.end('</ul>');
-            });
+            res.end('<p>Online: ' + n + '</p>');
         });
     });
     app.get('/logout', function(req, res){
