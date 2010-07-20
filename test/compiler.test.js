6812ab0b5a3f12b45b37bb2722155db101e2a8ee
Tj Holowaychuk <tj@vision-media.ca>
2010-07-20 14:32:43 -0700

    Compiler no longer 404s
    This will allow for vanialla css etc
    to be served, even though compiler took
    a stab at finding the equiv sass or less file

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -14,7 +14,7 @@ module.exports = {
             connect.compiler({ src: __dirname + '/fixtures', enable: ['sass'] }),
             connect.staticProvider(__dirname + '/fixtures')
         );
-        server.assertResponse('GET', '/doesnotexist.css', 404, 'Not Found');
+        server.assertResponse('GET', '/doesnotexist.css', 404, 'Cannot GET /doesnotexist.css');
         server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
         server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
         server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
