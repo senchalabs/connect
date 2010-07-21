af50b7bede9a15f7e2229e1efd1753a02b979fcc
Tj Holowaychuk <tj@vision-media.ca>
2010-07-21 07:49:25 -0700

    Hello world example listening

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -1,9 +1,11 @@
 // Note that even though this looks like a normal node http app, it's really a
 // valid connect app using the connect middleware stack.
 
-var Connect = require('../../lib/connect');
+var connect = require('../../lib/connect');
 
-module.exports = Connect.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World');
+var server = connect.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
 });
+
+server.listen(3000);
