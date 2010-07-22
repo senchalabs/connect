98f3193355ab165e2e2d4083ad4f7133c07296a7
Aaron Heckmann <aaron@clickdummy.net>
2010-07-22 02:08:04 -0400

    configurable staticFile lifetime

--- test/static.test.js
+++ test/static.test.js
@@ -30,36 +30,50 @@ module.exports = {
                 assert.equal('application/json', headers['content-type'], 'Test static with valid file Content-Type');
                 assert.equal(body.length, headers['content-length'], 'Test static with valid file Content-Length');
                 assert.ok(headers['last-modified'], 'Test static with valid file Last-Modified');
-                assert.ok(headers['cache-control'], 'Test static with valid file Cache-Control');
+                assert.ok(headers['cache-control'] == 'public max-age=31536000', 'Test static with valid file Cache-Control');
             });
         });
         req.end();
     },
-    
+
+    'test configurable cache-control': function(){
+        var server = helpers.run(
+            connect.staticProvider({ root: fixturesPath, lifetime: 60000 })
+        );
+        var req = server.request('GET', '/user.json');
+        req.buffer = true;
+        req.addListener('response', function(res){
+            res.addListener('end', function(){
+                assert.ok(res.headers['cache-control'] == 'public max-age=60000', 'Test configurable Cache-Control support');
+            });
+        });
+        req.end();
+    },
+
     'test urlencoded': function(){
         var server = helpers.run(
-            connect.staticProvider(fixturesPath)
+            connect.staticProvider({ root: fixturesPath })
         );
         server.assertResponse('GET', '/some%20text.txt', 200, 'whoop', 'Test urlencoded path');
     },
 
     'test index.html support': function(){
         var server = helpers.run(
-            connect.staticProvider(fixturesPath)
+            connect.staticProvider({ root: fixturesPath })
         );
         server.assertResponse('GET', '/', 200, '<p>Wahoo!</p>', 'Test static index.html support.');
     },
 
     'test index.html support when missing': function(){
         var server = helpers.run(
-            connect.staticProvider(__dirname)
+            connect.staticProvider({ root: __dirname })
         );
         server.assertResponse('GET', '/', 404, 'Cannot GET /', 'Test static index.html support when missing.');
     },
 
     'test invalid file': function(){
         var server = helpers.run(
-            connect.staticProvider(fixturesPath)
+            connect.staticProvider({ root: fixturesPath })
         );
         server.assertResponse('GET', '/foo.json', 404, 'Cannot GET /foo.json', 'Test invalid static file.');
     }
