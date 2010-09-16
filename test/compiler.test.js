5e499e6fbbe8b4301433a4d0c0f04498f205772a
Jacques Crocker <railsjedi@gmail.com>
2010-09-16 16:54:47 -0700

    Add Coffee-Script compiler tests

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -11,7 +11,7 @@ var connect = require('connect'),
 module.exports = {
     test: function(){
         var server = helpers.run(
-            connect.compiler({ src: __dirname + '/fixtures', enable: ['sass'] }),
+            connect.compiler({ src: __dirname + '/fixtures', enable: ['sass', 'coffeescript'] }),
             connect.staticProvider({ root: __dirname + '/fixtures' })
         );
         server.assertResponse('GET', '/doesnotexist.css', 404, 'Cannot GET /doesnotexist.css');
@@ -19,8 +19,9 @@ module.exports = {
         server.assertResponse('GET', '/style.css', 200, 'body {\n  font-size: 12px;\n  color: #000;}\n');
         server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
         server.assertResponse('GET', '/foo.bar.baz.css', 200, 'foo {\n  color: #000;}\n');
+        server.assertResponse('GET', '/script.js', 200, '(function() {\n  var $;\n  $ = jQuery;\n  $(document).ready(function() {\n    return console.log("Say Hello to CoffeeScript");\n  });\n})();\n');
     },
-    
+
     'test .compilers': function(){
         assert.equal('object', typeof connect.compiler.compilers);
     }
---
+++ test/fixtures/script.coffee
@@ -0,0 +1,3 @@
+$ = jQuery
+$(document).ready ->
+  console.log "Say Hello to CoffeeScript"
