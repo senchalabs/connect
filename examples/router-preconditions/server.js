0c2898b54ae45ed6ac215716966b958550c01e9f
Tj Holowaychuk <tj@vision-media.ca>
2010-12-06 16:23:06 -0800

    Added route segment precondition support and example

--- examples/router-preconditions/server.js
+++ examples/router-preconditions/server.js
@@ -3,14 +3,19 @@ var connect = require('./../../lib/connect')
   , server = connect.createServer();
 
 var users = [
-    { name: 'tj '}
-  , { name: 'tobi' }
-  , { name: 'jane' }
-  , { name: 'loki' }
+    { name: 'tj', tags: ['foo', 'bar', 'bar'] }
+  , { name: 'tobi', tags: ['ferret'] }
+  , { name: 'jane', tags: ['ferret'] }
+  , { name: 'loki', tags: ['ferret', 'fat'] }
 ];
 
 server.use(connect.router(function(app){
-  app.param('userId', function(req, res, next, id){
+  function asInt(n){ return parseInt(n, 10); }
+
+  app.param('from', asInt);
+  app.param('to', asInt);
+
+  app.param('user', function(req, res, next, id){
     if (/^[0-9]+$/.test(id)) {
       if (req.user = users[id]) {
         next();
@@ -22,9 +27,17 @@ server.use(connect.router(function(app){
     }
   });
 
-  app.get('/user/:userId', function(req, res){
+  app.get('/user/:user', function(req, res){
     res.writeHead(200);
     res.end('user ' + req.user.name);
+  });
+
+  app.get('/user/:user/tags/:from-:to', function(req, res){
+    var from = req.params.from
+      , to = req.params.to
+      , tags = req.user.tags.slice(from, to);
+    res.writeHead(200);
+    res.end('user has tags ' + tags.join(', '));
   });
 }));
 
