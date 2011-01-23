2323bd609b6352ddc56a53d096e950015b41bac4
Tj Holowaychuk <tj@vision-media.ca>
2011-01-23 00:21:54 -0800

    Added stylus support

--- examples/stylus/server.js
+++ examples/stylus/server.js
@@ -9,10 +9,11 @@ var connect = require('../../lib/connect');
 //   $ npm install stylus
 
 // To compile simply execute:
-//   $ curl http://localhost:3000/stylesheets/main.css
+//   $ curl http://localhost:3000/main.css
 
+var pub = __dirname + '/public';
 connect.createServer(
-    connect.compiler({ src: __dirname + '/public', enable: ['stylus'] }),
+    connect.compiler({ src: pub, enable: ['stylus'] }),
     connect.staticProvider(pub)
 ).listen(3000);
 console.log('Connect server listening on port 3000');
