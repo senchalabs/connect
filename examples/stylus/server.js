fa7cfabb9aabf571fa10fcd4bffaf4ddbfb70957
Tj Holowaychuk <tj@vision-media.ca>
2011-01-27 13:43:45 -0800

    Revert "Added stylus support"
    This reverts commit 2323bd609b6352ddc56a53d096e950015b41bac4.

--- examples/stylus/server.js
+++ examples/stylus/server.js
@@ -9,11 +9,10 @@ var connect = require('../../lib/connect');
 //   $ npm install stylus
 
 // To compile simply execute:
-//   $ curl http://localhost:3000/main.css
+//   $ curl http://localhost:3000/stylesheets/main.css
 
-var pub = __dirname + '/public';
 connect.createServer(
-    connect.compiler({ src: pub, enable: ['stylus'] }),
+    connect.compiler({ src: __dirname + '/public', enable: ['stylus'] }),
     connect.staticProvider(pub)
 ).listen(3000);
 console.log('Connect server listening on port 3000');
