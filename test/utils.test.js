613273496ac258a321549c668ebdbe3b3c6b2754
Tj Holowaychuk <tj@vision-media.ca>
2011-02-10 19:15:39 -0800

    refactor util tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,58 +3,43 @@
  * Module dependencies.
  */
 
-var utils = require('connect').utils,
-    assert = require('assert');
+var utils = require('connect').utils
+  , should = require('should');
 
 module.exports = {
     'test md5()': function(){
-        assert.equal('5JMpgGF2EjbJawLqaqiirQ==', utils.md5('wahoo', 'base64'));
-        assert.equal('e493298061761236c96b02ea6aa8a2ad', utils.md5('wahoo'));
+      utils.md5('wahoo', 'base64').should.equal('5JMpgGF2EjbJawLqaqiirQ==');
+      utils.md5('wahoo').should.equal('e493298061761236c96b02ea6aa8a2ad');
     },
 
     'test uid()': function(){
-        for(var i = 0; i < 100; ++i){
-          assert.length(utils.uid(i), i);
-        }
-    },
-
-    'test merge()': function(){
-        assert.eql({ foo: 'bar' }, utils.merge({ foo: 'bar' }));
-        assert.eql({ foo: 'bar', bar: 'baz' }, utils.merge({ foo: 'bar' }, { bar: 'baz' }));
-        assert.eql({ foo: 'baz' }, utils.merge({ foo: 'bar' }, { foo: 'baz' }));
-    },
-
-    'test toBoolean()': function(){
-        assert.strictEqual(true, utils.toBoolean(true));
-        assert.strictEqual(true, utils.toBoolean(1));
-        assert.strictEqual(true, utils.toBoolean('1'));
-        assert.strictEqual(true, utils.toBoolean('true'));
-        assert.strictEqual(true, utils.toBoolean('yes'));
-        assert.strictEqual(true, utils.toBoolean('y'));
-
-        assert.strictEqual(false, utils.toBoolean(false));
-        assert.strictEqual(false, utils.toBoolean(0));
-        assert.strictEqual(false, utils.toBoolean('0'));
-        assert.strictEqual(false, utils.toBoolean('-1'));
-        assert.strictEqual(false, utils.toBoolean('false'));
-        assert.strictEqual(false, utils.toBoolean('no'));
-        assert.strictEqual(false, utils.toBoolean('n'));
-        assert.strictEqual(false, utils.toBoolean('awrasdfasdfas'));
-        assert.strictEqual(false, utils.toBoolean(''));
+      for(var i = 0; i < 100; ++i){
+        utils.uid(i).should.have.length(i);
+      }
     },
 
     'test parseCookie()': function(){
-        assert.eql({ foo: 'bar' }, utils.parseCookie('foo=bar'));
-        assert.eql({ sid: '123' }, utils.parseCookie('SID=123'));
-        assert.eql({ sid: '123' }, utils.parseCookie('SID=123;SID=somethingElse'));
-        assert.eql({ foo: 'bar', baz: 'raz' }, utils.parseCookie('foo   =  bar; baz = raz'));
-        assert.eql({ fbs: 'uid=0987654321&name=Test User' }, utils.parseCookie('fbs="uid=0987654321&name=Test+User"'));
+      utils.parseCookie('foo=bar').should.eql({ foo: 'bar' });
+      utils.parseCookie('SID=123').should.eql({ sid: '123' });
+      utils.parseCookie('foo   = bar;  baz    =  raz').should.eql({ foo: 'bar', baz: 'raz' });  
+      utils.parseCookie('fbs="uid=0987654321&name=Test+User"').should.eql({ fbs: 'uid=0987654321&name=Test User' });
     },
 
     'test serializeCookie()': function(){
-        assert.equal('foo=bar; path=/', utils.serializeCookie('foo', 'bar', { path: '/' }));
-        assert.equal('foo=bar; secure', utils.serializeCookie('foo', 'bar', { secure: true }));
-        assert.equal('foo=bar', utils.serializeCookie('foo', 'bar', { secure: false }));
-        assert.equal('foo=foo%20bar', utils.serializeCookie('foo', 'foo bar'));
+      utils
+        .serializeCookie('foo', 'bar', { path: '/' })
+        .should.equal('foo=bar; path=/');
+
+      utils
+        .serializeCookie('foo', 'bar', { secure: true })
+        .should.equal('foo=bar; secure');
+
+      utils
+        .serializeCookie('foo', 'bar', { secure: false })
+        .should.equal('foo=bar');
+
+      utils
+        .serializeCookie('foo', 'foo bar')
+        .should.equal('foo=foo%20bar');
     }
 };
