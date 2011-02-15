9d26384583ddef8713f05b89e96ca984af7f8eab
Tj Holowaychuk <tj@vision-media.ca>
2011-02-14 16:26:14 -0800

    Added test for custom bodyParser

--- test/bodyParser.test.js
+++ test/bodyParser.test.js
@@ -46,5 +46,21 @@ module.exports = {
     assert.response(app,
       { url: '/', method: 'POST' },
       { body: '' });
+  },
+  
+  'test custom parser': function(){
+    connect.bodyParser.parse['application/x-awesome'] = function(str){
+      var obj = {}
+        , parts = str.split('.');
+      obj[parts.shift()] = parts.shift();
+      return obj;
+    };
+
+    assert.response(app,
+      { url: '/'
+      , method: 'POST'
+      , data: 'foo.bar'
+      , headers: { 'Content-Type': 'application/x-awesome' }},
+      { body: '{"foo":"bar"}' });
   }
 };
