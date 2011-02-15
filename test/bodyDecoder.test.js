6fd0b12f7c152f84bc2a473fa13ff4bbcf2f3d08
Tj Holowaychuk <tj@vision-media.ca>
2011-02-14 16:22:32 -0800

    Fixed bodyDecoder tests

--- test/bodyDecoder.test.js
+++ test/bodyDecoder.test.js
@@ -4,88 +4,47 @@
  */
 
 var connect = require('connect'),
-    helpers = require('./helpers'),
     assert = require('assert'),
     http = require('http');
 
+var app = connect.createServer(
+  connect.bodyDecoder(),
+  function(req, res){
+  res.writeHead(200);
+  res.end(JSON.stringify(req.body));
+});
+
 module.exports = {
-    'test urlencoded body': function(){
-        var server = helpers.run(
-            connect.bodyDecoder(),
-            function(req, res){
-                assert.eql({ user: { name: 'tj' }}, req.body, 'Test body-decoder urlencoded req.body');
-                res.writeHead(200);
-                res.end();
-            }
-        );
-        var req = server.request('POST', '/', { 'Content-Type': 'application/x-www-form-urlencoded' });
-        req.write('user[name]=tj');
-        req.end();
-    },
+  'test x-www-form-urlencoded body': function(){
+    assert.response(app,
+      { url: '/'
+      , data: 'foo=bar'
+      , method: 'POST'
+      , headers: { 'Content-Type': 'application/x-www-form-urlencoded' }},
+      { body: '{"foo":"bar"}' });
+  },
+  
+  'test PUT x-www-form-urlencoded body': function(){
+    assert.response(app,
+      { url: '/'
+      , data: 'foo=bar'
+      , method: 'PUT'
+      , headers: { 'Content-Type': 'application/x-www-form-urlencoded' }},
+      { body: '{"foo":"bar"}' });
+  },
+  
+  'test json body': function(){
+    assert.response(app,
+      { url: '/'
+      , data: '{"foo":"bar"}'
+      , method: 'PUT'
+      , headers: { 'Content-Type': 'application/json' }},
+      { body: '{"foo":"bar"}' });
+  },
 
-    'test json body': function(){
-        var server = helpers.run(
-            connect.bodyDecoder(),
-            function(req, res){
-                assert.eql({ user: { name: 'tj' }}, req.body, 'Test body-decoder json req.body');
-                res.writeHead(200);
-                res.end();
-            }
-        );
-        var req = server.request('POST', '/', { 'Content-Type': 'application/json; charset=utf8' });
-        req.write('{"user":{"name":"tj"}}');
-        req.end();
-    },
-
-    'test empty body': function(){
-        var server = helpers.run(
-            connect.bodyDecoder(),
-            function(req, res){
-                assert.eql({}, req.body, 'Test body-decoder empty req.body');
-                res.writeHead(200);
-                res.end();
-            }
-        );
-        var req = server.request('POST', '/', { 'Content-Type': 'application/json; charset=utf8' });
-        req.write('');
-        req.end();
-    },
-
-    'test invalid body': function(){
-        var server = helpers.run(
-            connect.bodyDecoder(),
-            function(err, req, res, next){
-                assert.equal('unexpected_eos', err.type, 'Test body-decoder invalid json error');
-                assert.equal('{"user":{"name":"tj"', req.rawBody, 'Test body-decoder req.rawBody');
-                assert.strictEqual(undefined, req.body, 'Test body-decoder invalid json req.body');
-                res.writeHead(200);
-                res.end();
-            }
-        );
-        var req = server.request('POST', '/', { 'Content-Type': 'application/json' });
-        req.write('{"user":{"name":"tj"');
-        req.end();
-    },
-    
-    'test custom decoder': function(){
-        connect.bodyDecoder.decode['my/type'] = function(str){
-            return str.replace(/ +/g, '');
-        };
-        var server = helpers.run(
-            connect.bodyDecoder(),
-            function(req, res){
-                res.writeHead(200);
-                res.end(req.body);
-            }
-        );
-        var req = server.request('POST', '/', { 'Content-Type': 'my/type' });
-        req.buffer = true;
-        req.on('response', function(res){
-            res.on('end', function(){
-                assert.equal('foobar', res.body, 'Test custom decoder function');
-            });
-        });
-        req.write('f o o b a r');
-        req.end();
-    }
-}
+  'test POST with no data': function(){
+    assert.response(app,
+      { url: '/', method: 'POST' },
+      { body: '' });
+  }
+};
