194ee9d847e8656ccb469c46ba9ddd6b92dfa8a5
Tj Holowaychuk <tj@vision-media.ca>
2011-02-14 16:30:51 -0800

    better cookieParser tests

--- test/cookieParser.test.js
+++ test/cookieParser.test.js
@@ -7,22 +7,29 @@ var connect = require('connect')
   , assert = require('assert')
   , http = require('http');
 
-module.exports = {
-  test: function(){
-    var n = 0;
-    var app = connect.createServer(
-      connect.cookieParser(),
-      function(req, res, next){
-        res.end(JSON.stringify(req.cookies));
-      }
-    );
+var app = connect.createServer(
+  connect.cookieParser(),
+  function(req, res, next){
+    res.end(JSON.stringify(req.cookies));
+  }
+);
 
+module.exports = {
+  'test without cookies': function(){
     assert.response(app,
       { url: '/' },
       { body: '{}' });
-    
+  },
+  
+  'test single cookie': function(){
     assert.response(app,
-      { url: '/', headers: { Cookie: ['sid=123', 'foo=bar'] }},
-      { body: '{"sid":"123","foo":"bar"}' });
+      { url: '/', headers: { Cookie: ['sid=123'] }},
+      { body: '{"sid":"123"}' });
+  },
+  
+  'test several cookies': function(){
+    assert.response(app,
+      { url: '/', headers: { Cookie: ['sid=123', 'name=tj'] }},
+      { body: '{"sid":"123","name":"tj"}' });
   }
 };
