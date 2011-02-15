6643df7f3d936bc87fab36dd4a3f1315294a6db8
Tj Holowaychuk <tj@vision-media.ca>
2011-02-14 19:21:36 -0800

    refactored errorHandler

--- examples/error/server.js
+++ examples/error/server.js
@@ -6,10 +6,10 @@
 var connect = require('../../lib/connect');
 
 var server = connect.createServer(
-    function(req, res, next){
-        throw new Error('oh noes!');
-    },
-    connect.errorHandler({ showStack: true, dumpExceptions: true })
+  function(req, res, next){
+    throw new Error('oh noes!');
+  },
+  connect.errorHandler({ showStack: true, dumpExceptions: true })
 );
 
 server.listen(3000);
