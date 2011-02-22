3acd70b2d6b013a50097027665dcdb3f74e1db83
Tj Holowaychuk <tj@vision-media.ca>
2011-02-22 12:49:40 -0800

    Added parseRange() util

--- test/utils.test.js
+++ test/utils.test.js
@@ -86,5 +86,16 @@ module.exports = {
     defer(function(){
       calls.should.equal(3);
     });
+  },
+  
+  'test .parseRange()': function(){
+    utils.parseRange(1000, 'bytes=0-499').should.eql([{ start: 0, end: 499 }]);
+    utils.parseRange(1000, 'bytes=40-80').should.eql([{ start: 40, end: 80 }]);
+    utils.parseRange(1000, 'bytes=-500').should.eql([{ start: 500, end: 999 }]);
+    utils.parseRange(1000, 'bytes=-400').should.eql([{ start: 600, end: 999 }]);
+    utils.parseRange(1000, 'bytes=500-').should.eql([{ start: 500, end: 999 }]);
+    utils.parseRange(1000, 'bytes=400-').should.eql([{ start: 400, end: 999 }]);
+    utils.parseRange(1000, 'bytes=0-0').should.eql([{ start: 0, end: 0 }]);
+    utils.parseRange(1000, 'bytes=-1').should.eql([{ start: 999, end: 999 }]);
   }
 };
