448a670633dcdae99eeac3dfee4d4b84f6c0a62f
Tj Holowaychuk <tj@vision-media.ca>
2011-02-25 16:11:10 -0800

    Added redis session example

--- examples/session.js
+++ examples/session.js
@@ -22,7 +22,7 @@ connect(
     }
   }
 ).listen(3000);
-console.log('server listening on port 3000');
+console.log('port 3000: 1 minute expiration demo');
 
 // session cookie example
 // existing as long as the browser
@@ -44,4 +44,40 @@ connect(
     }
   }
 ).listen(3001);
-console.log('server listening on port 3001');
+console.log('port 3001: session cookies');
+
+// $ npm install connect-redis
+
+try {
+  var RedisStore = require('connect-redis');
+  connect(
+      connect.cookieParser()
+    , connect.session({
+        secret: 'keyboard cat'
+      , cookie: { maxAge: 60000 * 3 }
+      , store: new RedisStore
+    })
+    , connect.favicon()
+    , function(req, res, next){
+      var sess = req.session;
+      if (sess.views) {
+        sess.views++;
+        res.end('<p>views: ' + sess.views + '</p>');
+      } else {
+        sess.views = 1;
+        sess.cookie.expires = false;
+        res.end('welcome to the redis demo. refresh!');
+      }
+    }
+  ).listen(3002);
+  console.log('port 3002: redis example');
+} catch (err) {
+  console.log();
+  console.log('failed to start the Redis example.');
+  console.log('to try it install redis, start redis');
+  console.log('install connect-redis, and run this');
+  console.log('script again.');
+  console.log('    $ redis-server');
+  console.log('    $ npm install connect-redis');
+  console.log();
+}
