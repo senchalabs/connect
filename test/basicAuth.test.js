232d960806dddd93a90beed92912c6f1539474c4
Tj Holowaychuk <tj@vision-media.ca>
2011-03-28 08:27:32 -0700

    started async basicAuth tests

--- test/basicAuth.test.js
+++ test/basicAuth.test.js
@@ -7,9 +7,32 @@ var connect = require('connect')
   , assert = require('assert')
   , http = require('http');
 
-var app = connect.createServer(
+// sync
+
+var app = connect(
   connect.basicAuth(function(user, pass){
     return 'tj' == user && 'tobi' == pass;
+  }),
+  function(req, res){
+    res.end('wahoo');
+  }
+);
+
+// async
+
+var User = {
+  find: function(query, fn){
+    if (query.name == 'tj' && query.pass == 'tobi') {
+      fn(null, { name: 'tj' });
+    } else {
+      fn(new Error('user not found'));
+    }
+  }
+}
+
+var async = connect(
+  connect.basicAuth(function(user, pass, fn){
+    User.find({ name: user, pass: pass }, fn);
   }),
   function(req, res){
     res.end('wahoo');
@@ -43,5 +66,11 @@ module.exports = {
     assert.response(app,
       { url: '/', headers: { Authorization: 'Foo asdfasdf' }},
       { body: 'Bad Request', status: 400 });
-  }
+  },
+  
+  'test async authorized': function(){
+    assert.response(async,
+      { url: '/', headers: { Authorization: 'Basic dGo6dG9iaQ==' }},
+      { body: 'wahoo', status: 200 });
+  },
 };
