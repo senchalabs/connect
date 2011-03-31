60906cc4abdde074cbfe245d8a1adc7f3cee2498
Alexander Simmerl <alx@soundcloud.com>
2011-04-01 00:07:42 +0200

    reduced dependencies for logger tests

--- test/logger.test.js
+++ test/logger.test.js
@@ -9,35 +9,34 @@ var connect = require('connect')
   , https = require('https')
   , fs = require('fs');
 
-var fixtures = __dirname + '/fixtures'
-  , logPath = __dirname + '/fixtures/foo.log';
-
 module.exports = {
   'test http remoteAddress logging': function(){
-    var app = connect.createServer(
-      connect.logger({
-        format: ':remote-addr',
-        stream: fs.createWriteStream(logPath)
-      })
-    );
+    var logLine = ''
+      , app = connect.createServer(
+          connect.logger({
+            format: ':remote-addr',
+            stream: { write: function(line){ logLine = line; } }
+          })
+        );
 
     assert.response(app,
       { url: '/'},
       function(){
-        assert.equal(fs.readFileSync(logPath).toString(), '127.0.0.1\n');
+        assert.equal(logLine, '127.0.0.1\n');
       });
   },
   'test https remoteAddress logging': function(){
-    var app = connect.createServer(
-      {
-        key: fs.readFileSync(__dirname + '/fixtures/ssl.key'),
-        cert: fs.readFileSync(__dirname + '/fixtures/ssl.crt')
-      },
-      connect.logger({
-        format: ':remote-addr',
-        stream: fs.createWriteStream(logPath)
-      })
-    );
+    var logLine = ''
+      , app = connect.createServer(
+          {
+            key: fs.readFileSync(__dirname + '/fixtures/ssl.key'),
+            cert: fs.readFileSync(__dirname + '/fixtures/ssl.crt')
+          },
+          connect.logger({
+            format: ':remote-addr',
+            stream: { write: function(line){ logLine = line; } }
+          })
+        );
 
     app.listen(7777, '127.0.0.1', function() {
       var request = https.request({
@@ -56,7 +55,7 @@ module.exports = {
           var status = 404;
 
           assert.equal(response.statusCode, status);
-          assert.equal(fs.readFileSync(logPath).toString(), '127.0.0.1\n');
+          assert.equal(logLine, '127.0.0.1\n');
 
           app.close();
         });
