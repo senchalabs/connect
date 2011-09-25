b790ba23c88b1afde5f8f77805d39ecd6d2d37ad
Ryan Olds <ryanrolds@gmail.com>
2011-09-25 15:23:13 -0500

    Added test for conditional range request

--- test/static.test.js
+++ test/static.test.js
@@ -183,6 +183,18 @@ module.exports = {
       { url: '/list', headers: { Range: 'bytes=3-6' }},
       { body: '4567', 'Content-Range': 'bytes 3-6/9', status: 206 });
   },
+
+  'test conditional range': function(){
+    assert.response(app,
+      { url: '/list' },
+      function(res) {
+        res.headers.should.have.property('etag');
+        assert.response(app,
+          { url: '/list', headers: { Range: 'bytes=3-6', 'If-None-Match': res.headers.etag }},
+          { status: 304 }
+        );           
+      });    
+  },
   
   'test invalid Range': function(){
     assert.response(app,
