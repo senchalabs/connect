12de69d8372f926725cf77c761b0a6dcd07e6102
Tj Holowaychuk <tj@vision-media.ca>
2011-11-11 20:35:32 -0800

    Added basicAuth() tests for alternate signatures

--- test/basicAuth.js
+++ test/basicAuth.js
@@ -2,68 +2,82 @@
 var connect = require('../')
   , request = require('./support/http');
 
-describe('connect.basicAuth(user, pass)', function(){
-  describe('when missing Authorization', function(){
-    it('should respond with WWW-Authenticate', function(done){
-      var app = connect();
+function test(app, signature) {
+  describe(signature, function(){
+    describe('when missing Authorization', function(){
+      it('should respond with 401 and WWW-Authenticate', function(done){
+        request(app)
+        .get('/')
+        .end(function(res){
+          res.statusCode.should.equal(401);
+          res.headers['www-authenticate'].should.equal('Basic realm="Authorization Required"');
+          done();
+        });
+      })
+    })
 
-      app.use(connect.basicAuth('tj', 'tobi'));
+    describe('when valid', function(){
+      it('should next()', function(done){
+        request(app)
+        .get('/')
+        .set('Authorization', 'Basic dGo6dG9iaQ==')
+        .end(function(res){
+          res.statusCode.should.equal(200);
+          res.body.should.equal('secret!');
+          done();
+        });
+      })
+    })
 
-      app.use(function(req, res, next){
-        res.end('secret!');
-      });
-
-      request(app)
-      .get('/')
-      .end(function(res){
-        res.statusCode.should.equal(401);
-        res.headers['www-authenticate'].should.equal('Basic realm="Authorization Required"');
-        done();
-      });
+    describe('when invalid', function(){
+      it('should respond with 401', function(done){
+        request(app)
+        .get('/')
+        .set('Authorization', 'Basic dGo69iaQ==')
+        .end(function(res){
+          res.statusCode.should.equal(401);
+          res.headers['www-authenticate'].should.equal('Basic realm="Authorization Required"');
+          res.body.should.equal('Unauthorized');
+          done();
+        });
+      })
     })
   })
-  
-  describe('when valid', function(){
-    it('should next()', function(done){
-      var app = connect();
+}
 
-      app.use(connect.basicAuth('tj', 'tobi'));
+var app = connect();
 
-      app.use(function(req, res, next){
-        res.end('secret!');
-      });
+app.use(connect.basicAuth('tj', 'tobi'));
 
-      request(app)
-      .get('/')
-      .set('Authorization', 'Basic dGo6dG9iaQ==')
-      .end(function(res){
-        res.statusCode.should.equal(200);
-        res.body.should.equal('secret!');
-        done();
-      });
-    })
-  })
-  
-  describe('when invalid', function(){
-    it('should next()', function(done){
-      var app = connect();
+app.use(function(req, res, next){
+  res.end('secret!');
+});
 
-      app.use(connect.basicAuth('tj', 'tobi'));
+test(app, 'connect.basicAuth(user, pass)');
 
-      app.use(function(req, res, next){
-        res.end('secret!');
-      });
 
-      request(app)
-      .get('/')
-      .set('Authorization', 'Basic dGo69iaQ==')
-      .end(function(res){
-        res.statusCode.should.equal(401);
-        res.headers['www-authenticate'].should.equal('Basic realm="Authorization Required"');
-        res.body.should.equal('Unauthorized');
-        done();
-      });
-    })
-  })
 
-})
+var app = connect();
+
+app.use(connect.basicAuth(function(user, pass){
+  return 'tj' == user && 'tobi' == pass;
+}));
+
+app.use(function(req, res, next){
+  res.end('secret!');
+});
+
+test(app, 'connect.basicAuth(callback)');
+
+
+var app = connect();
+
+app.use(connect.basicAuth(function(user, pass, fn){
+  fn(null, 'tj' == user && 'tobi' == pass);
+}));
+
+app.use(function(req, res, next){
+  res.end('secret!');
+});
+
+test(app, 'connect.basicAuth(callback) async');
