070aaec597559d5e49515cef67e0147076d35ecd
Tj Holowaychuk <tj@vision-media.ca>
2011-11-28 09:32:57 -0800

    version test

--- test/exports.js
+++ test/exports.js
@@ -2,6 +2,12 @@
 var connect = require('../');
 
 describe('exports', function(){
+  describe('.version', function(){
+    it('should be a string', function(){
+      connect.version.should.be.a('string');
+    })
+  })
+
   describe('.middleware', function(){
     it('should lazy-load middleware', function(){
       connect.middleware.cookieParser.should.be.a('function');
