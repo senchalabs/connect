976d95c4f51a6e3edb5953abb1fb57042c990402
Tj Holowaychuk <tj@vision-media.ca>
2011-11-28 16:02:00 -0800

    Added `multipart()` middleware and tests

---
+++ test/multipart.js
@@ -0,0 +1,251 @@
+
+var connect = require('../')
+  , should = require('./shared');
+
+var app = connect();
+
+app.use(connect.multipart());
+
+app.use(function(req, res){
+  res.end(JSON.stringify(req.body));
+});
+
+describe('connect.bodyParser()', function(){
+  should['default request body'](app);
+
+  it('should ignore GET', function(done){
+    app.request()
+    .get('/')
+    .set('Content-Type', 'multipart/form-data; boundary=foo')
+    .write('--foo\r\n')
+    .write('Content-Disposition: form-data; name="user"\r\n')
+    .write('\r\n')
+    .write('Tobi')
+    .write('\r\n--foo--')
+    .end(function(res){
+      res.body.should.equal('{}');
+      done();
+    });
+  })
+
+  describe('with multipart/form-data', function(){
+    it('should populate req.body', function(done){
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="user"\r\n')
+      .write('\r\n')
+      .write('Tobi')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.body.should.equal('{"user":"Tobi"}');
+        done();
+      });
+    })
+    
+    it('should support files', function(done){
+      var app = connect();
+
+      app.use(connect.bodyParser());
+
+      app.use(function(req, res){
+        req.body.user.should.eql({ name: 'Tobi' });
+        req.body.text.path.should.not.include.string('.txt');
+        req.body.text.constructor.name.should.equal('File');
+        res.end(req.body.text.name);
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="user[name]"\r\n')
+      .write('\r\n')
+      .write('Tobi')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="text"; filename="foo.txt"\r\n')
+      .write('\r\n')
+      .write('some text here')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.body.should.equal('foo.txt');
+        done();
+      });
+    })
+    
+    it('should expose options to formidable', function(done){
+      var app = connect();
+
+      app.use(connect.bodyParser({
+        keepExtensions: true
+      }));
+
+      app.use(function(req, res){
+        req.body.user.should.eql({ name: 'Tobi' });
+        req.body.text.path.should.include.string('.txt');
+        req.body.text.constructor.name.should.equal('File');
+        res.end(req.body.text.name);
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="user[name]"\r\n')
+      .write('\r\n')
+      .write('Tobi')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="text"; filename="foo.txt"\r\n')
+      .write('\r\n')
+      .write('some text here')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.body.should.equal('foo.txt');
+        done();
+      });
+    })
+    
+    it('should work with multiple fields', function(done){
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="user"\r\n')
+      .write('\r\n')
+      .write('Tobi')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="age"\r\n')
+      .write('\r\n')
+      .write('1')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.body.should.equal('{"user":"Tobi","age":"1"}');
+        done();
+      });
+    })
+    
+    it('should support nesting', function(done){
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="user[name][first]"\r\n')
+      .write('\r\n')
+      .write('tobi')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="user[name][last]"\r\n')
+      .write('\r\n')
+      .write('holowaychuk')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="user[age]"\r\n')
+      .write('\r\n')
+      .write('1')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="species"\r\n')
+      .write('\r\n')
+      .write('ferret')
+      .write('\r\n--foo--')
+      .end(function(res){
+        var obj = JSON.parse(res.body);
+        obj.user.age.should.equal('1');
+        obj.user.name.should.eql({ first: 'tobi', last: 'holowaychuk' });
+        obj.species.should.equal('ferret');
+        done();
+      });
+    })
+
+    it('should support multiple files of the same name', function(done){
+      var app = connect();
+
+      app.use(connect.bodyParser());
+
+      app.use(function(req, res){
+        req.body.text.should.have.length(2);
+        req.body.text[0].constructor.name.should.equal('File');
+        req.body.text[1].constructor.name.should.equal('File');
+        res.end();
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="text"; filename="foo.txt"\r\n')
+      .write('\r\n')
+      .write('some text here')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n')
+      .write('\r\n')
+      .write('some more text stuff')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.statusCode.should.equal(200);
+        done();
+      });
+    })
+    
+    it('should support nested files', function(done){
+      var app = connect();
+
+      app.use(connect.bodyParser());
+
+      app.use(function(req, res){
+        Object.keys(req.body.docs).should.have.length(2);
+        req.body.docs.foo.name.should.equal('foo.txt');
+        req.body.docs.bar.name.should.equal('bar.txt');
+        res.end();
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-Disposition: form-data; name="docs[foo]"; filename="foo.txt"\r\n')
+      .write('\r\n')
+      .write('some text here')
+      .write('\r\n--foo\r\n')
+      .write('Content-Disposition: form-data; name="docs[bar]"; filename="bar.txt"\r\n')
+      .write('\r\n')
+      .write('some more text stuff')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.statusCode.should.equal(200);
+        done();
+      });
+    })
+    
+    it('should next(err) on multipart failure', function(done){
+      var app = connect();
+
+      app.use(connect.bodyParser());
+
+      app.use(function(req, res){
+        res.end('whoop');
+      });
+
+      app.use(function(err, req, res, next){
+        err.message.should.equal('parser error, 16 of 28 bytes parsed');
+        res.statusCode = 500;
+        res.end();
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'multipart/form-data; boundary=foo')
+      .write('--foo\r\n')
+      .write('Content-filename="foo.txt"\r\n')
+      .write('\r\n')
+      .write('some text here')
+      .write('Content-Disposition: form-data; name="text"; filename="bar.txt"\r\n')
+      .write('\r\n')
+      .write('some more text stuff')
+      .write('\r\n--foo--')
+      .end(function(res){
+        res.statusCode.should.equal(500);
+        done();
+      });
+    })
+
+  })
+})
