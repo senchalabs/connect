630d9cb0c6653fd1b7530901b45475eaef0e56c0
Tj Holowaychuk <tj@vision-media.ca>
2011-12-06 14:53:35 -0800

    started mounting tests

---
+++ test/mounting.js
@@ -0,0 +1,42 @@
+
+var connect = require('../');
+
+describe('app.use()', function(){
+  var app;
+
+  beforeEach(function(){
+    app = connect();
+  })
+
+  describe('with a connect app', function(){
+    it('should mount', function(done){
+      var blog = connect();
+
+      blog.use(function(req, res){
+        req.url.should.equal('/');
+        res.end('blog');
+      });
+
+      app.use('/blog', blog);
+
+      app.request()
+      .get('/blog')
+      .expect('blog', done);
+    })
+  })
+})
+
+// describe('app.route', function(){
+//   it('should return the mounted path', function(){
+//     var app = express()
+//       , blog = express()
+//       , blogAdmin = express();
+// 
+//     app.use('/blog', blog);
+//     blog.use('/admin', blogAdmin);
+// 
+//     app.route.should.equal('/');
+//     blog.route.should.equal('/blog');
+//     blogAdmin.route.should.equal('/admin');
+//   })
+// })
