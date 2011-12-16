2e0f6db809a3c12da6ae1558d8fd55ab916f8b51
Tj Holowaychuk <tj@vision-media.ca>
2011-12-16 15:49:15 -0800

    first session test

--- test/session.js
+++ test/session.js
@@ -1,5 +1,6 @@
 
-var connect = require('../');
+var connect = require('../')
+  , request = require('./support/http');
 
 function respond(req, res) {
   res.end(JSON.stringify(req.session));
@@ -19,5 +20,15 @@ describe('connect.session()', function(){
     connect.session.Session.should.be.a('function');
     connect.session.Store.should.be.a('function');
     connect.session.MemoryStore.should.be.a('function');
+  })
+
+  it('should Set-Cookie', function(done){
+    app.request()
+    .get('/')
+    .end(function(res){
+      res.headers['set-cookie'].should.have.length(1);
+      res.headers['set-cookie'][0].should.include('connect.sid');
+      done();
+    });
   })
 })
