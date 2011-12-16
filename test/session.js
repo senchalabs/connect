b12a71a07481ff413827fa7215c0e154b9b63803
Tj Holowaychuk <tj@vision-media.ca>
2011-12-16 15:43:45 -0800

    export constructors test

--- test/session.js
+++ test/session.js
@@ -5,11 +5,19 @@ function respond(req, res) {
   res.end(JSON.stringify(req.session));
 }
 
+function sid(res) {
+  return /^connect\.sid=([^;]+);/.exec(res.headers['set-cookie'][0])[1];
+}
+
 var app = connect()
   .use(connect.cookieParser('keyboard cat'))
   .use(connect.session())
   .use(respond);
 
 describe('connect.session()', function(){
-  
+  it('should export constructors', function(){
+    connect.session.Session.should.be.a('function');
+    connect.session.Store.should.be.a('function');
+    connect.session.MemoryStore.should.be.a('function');
+  })
 })
