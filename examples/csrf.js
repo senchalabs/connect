cfb01cfd410c0248665d4529f9f16ccfd966adec
TJ Holowaychuk <tj@vision-media.ca>
2012-01-22 11:20:07 -0800

    instrumented cookieSession() / session() with debug()

--- examples/csrf.js
+++ examples/csrf.js
@@ -3,34 +3,34 @@
  * Module dependencies.
  */
 
-var connect = require('../');
+var connect = require('../')
+  , http = require('http');
 
 var form = '\n\
   <form action="/" method="post">\n\
     <input type="hidden" name="_csrf" value="{token}" />\n\
-    <input type="text" name="user[name]" value="{user}" />\n\
-    <input type="password" name="user[pass]" />\n\
+    <input type="text" name="user[name]" value="{user}" placeholder="Username" />\n\
     <input type="submit" value="Login" />\n\
   </form>\n\
 '; 
 
-connect(
-    connect.cookieParser()
-  , connect.session({ secret: 'keyboard cat' })
-  , connect.bodyParser()
-  , connect.csrf()
-  
-  , function(req, res, next){
+var app = connect()
+  .use(connect.cookieParser('keyboard cat'))
+  .use(connect.session())
+  .use(connect.bodyParser())
+  .use(connect.csrf())
+  .use(function(req, res, next){
     if ('POST' != req.method) return next();
     req.session.user = req.body.user;
     next();
-  }
-
-  , function(req, res){
+  })
+  .use(function(req, res){
     res.setHeader('Content-Type', 'text/html');
     var body = form
       .replace('{token}', req.session._csrf)
       .replace('{user}', req.session.user && req.session.user.name || '');
     res.end(body);
-  }
-).listen(3000);
+  });
+
+http.createServer(app).listen(3000);
+console.log('Server listening on port 3000');
