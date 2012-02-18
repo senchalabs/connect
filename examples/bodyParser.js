5ddbef6081bbfc2d915d5372a19a4dd6227c3883
TJ Holowaychuk <tj@vision-media.ca>
2012-02-18 14:20:03 -0800

    tweak bodyParser example

--- examples/bodyParser.js
+++ examples/bodyParser.js
@@ -7,30 +7,34 @@ var connect = require('../')
 var app = connect()
   .use(connect.static(__dirname + '/public'))
   .use(connect.bodyParser())
-  .use(function(req, res, next){
-    if ('GET' != req.method) return next();
-    res.statusCode = 302;
-    res.setHeader('Location', 'form.html');
-    res.end();
-  })
-  .use(function(req, res){
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<p>thanks ' + req.body.name + '</p>');
-    res.write('<ul>');
+  .use(form)
+  .use(upload);
 
-    if (Array.isArray(req.files.images)) {
-      req.files.images.forEach(function(image){
-        var kb = image.size / 1024 | 0;
-        res.write('<li>uploaded ' + image.name + ' ' + kb + 'kb</li>');
-      });
-    } else {
-      var image = req.files.images;
+function form(req, res, next){
+  if ('GET' != req.method) return next();
+  res.statusCode = 302;
+  res.setHeader('Location', 'form.html');
+  res.end();
+}
+
+function upload(req, res){
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<p>thanks ' + req.body.name + '</p>');
+  res.write('<ul>');
+
+  if (Array.isArray(req.files.images)) {
+    req.files.images.forEach(function(image){
       var kb = image.size / 1024 | 0;
       res.write('<li>uploaded ' + image.name + ' ' + kb + 'kb</li>');
-    }
+    });
+  } else {
+    var image = req.files.images;
+    var kb = image.size / 1024 | 0;
+    res.write('<li>uploaded ' + image.name + ' ' + kb + 'kb</li>');
+  }
 
-    res.end('</ul>');
-  });
+  res.end('</ul>');
+}
 
 http.Server(app).listen(3000);
 console.log('Server started on port 3000');
