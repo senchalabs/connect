6747ecc1c85499f067ad72ce8bfdc4f8b4a7f930
TJ Holowaychuk <tj@vision-media.ca>
2012-02-27 17:21:25 -0800

    Added `app.listen()` for backwards compat
    -ish

---
+++ test/app.listen.js
@@ -0,0 +1,18 @@
+
+var connect = require('../');
+
+describe('app.listen()', function(){
+  it('should wrap in an http.Server', function(done){
+    var app = connect();
+
+    app.use(function(req, res){
+      res.end();
+    });
+
+    app.listen(5555, function(){
+      app
+      .request('/')
+      .expect(200, done);
+    });
+  })
+})
