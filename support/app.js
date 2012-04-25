74fb3ac7b54932f6d2679fc431f9276042be4694
TJ Holowaychuk <tj@vision-media.ca>
2012-04-25 15:30:03 -0700

    more benchmarks

--- support/app.js
+++ support/app.js
@@ -11,4 +11,36 @@ app.use(function(req, res, next){
   }
 });
 
+var n = 9;
+while (n--) {
+  app.use(function(req, res, next){
+    next();
+  });
+}
+
+app.use(function(req, res, next){
+  if ('/10' == req.url) {
+    res.setHeader('Content-Length', 5);
+    res.end('Hello');
+  } else {
+    next();
+  }
+});
+
+var n = 9;
+while (n--) {
+  app.use(function(req, res, next){
+    next();
+  });
+}
+
+app.use(function(req, res, next){
+  if ('/40' == req.url) {
+    res.setHeader('Content-Length', 5);
+    res.end('Hello');
+  } else {
+    next();
+  }
+});
+
 app.listen(8000);
