518242a1458807a265a54a38c0a0c975fb997be5
TJ Holowaychuk <tj@vision-media.ca>
2012-06-11 09:09:21 -0700

    Replaced utils.parseRange() with range-parser module

--- test/utils.js
+++ test/utils.js
@@ -55,19 +55,6 @@ describe('utils.[un]sign()', function(){
   })
 })
 
-describe('utils.parseRange(len, str)', function(){
-  it('should parse range strings', function(){
-    utils.parseRange(1000, 'bytes=0-499').should.eql([{ start: 0, end: 499 }]);
-    utils.parseRange(1000, 'bytes=40-80').should.eql([{ start: 40, end: 80 }]);
-    utils.parseRange(1000, 'bytes=-500').should.eql([{ start: 500, end: 999 }]);
-    utils.parseRange(1000, 'bytes=-400').should.eql([{ start: 600, end: 999 }]);
-    utils.parseRange(1000, 'bytes=500-').should.eql([{ start: 500, end: 999 }]);
-    utils.parseRange(1000, 'bytes=400-').should.eql([{ start: 400, end: 999 }]);
-    utils.parseRange(1000, 'bytes=0-0').should.eql([{ start: 0, end: 0 }]);
-    utils.parseRange(1000, 'bytes=-1').should.eql([{ start: 999, end: 999 }]);
-  })
-})
-
 describe('utils.mime(req)', function(){
   it('should return the mime-type from Content-Type', function(){
     utils.mime({ headers: { 'content-type': 'text/html; charset=utf8' }})
