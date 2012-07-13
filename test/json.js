2619de8f60ca9110418857ba30343a221480e01f
TJ Holowaychuk <tj@vision-media.ca>
2012-07-13 11:11:21 -0700

    add more descriptive invalid json error message

--- test/json.js
+++ test/json.js
@@ -103,6 +103,27 @@ describe('connect.json()', function(){
     })
   })
 
+  describe('when strict is true', function(){
+    it('should not parse primitives', function(done){
+      var app = connect();
+      app.use(connect.json({ strict: true }));
+
+      app.use(function(req, res){
+        res.end(JSON.stringify(req.body));
+      });
+
+      app.request()
+      .post('/')
+      .set('Content-Type', 'application/json')
+      .write('true')
+      .end(function(res){
+        res.should.have.status(400);
+        res.body.should.include('invalid json');
+        done();
+      });
+    })
+  })
+
   describe('by default', function(){
     it('should 400 on primitives', function(done){
       var app = connect();
