2102da9d6c504e4bc5687375cbb238b513b040e8
Anuj Thapliyal <athapliyal@yp.com>
2013-09-13 11:43:23 -0700

    #893 Test cases for Set-Cookie header

--- test/patch.js
+++ test/patch.js
@@ -82,5 +82,34 @@ describe('patch', function(){
       })
     })
 
+    describe('should handle set-cookie setting multiple cookies', function(){
+        it('should be emitted', function(done){
+            var app = connect();
+
+            app.use(function(req, res, next){
+                res.on('header', function(){
+                    res.setHeader('Set-Cookie', ['foo1', 'bar1']);
+                    res.setHeader('Set-Cookie', ['foo2', 'bar2']);
+                });
+
+                next();
+            });
+
+            app.use(function(req, res){
+                res.end();
+            })
+
+            app.request()
+            .get('/')
+            .end(function(res){
+                res.headers['set-cookie'].should.include('foo1');
+                res.headers['set-cookie'].should.include('bar1');
+                res.headers['set-cookie'].should.include('foo2');
+                res.headers['set-cookie'].should.include('bar2');
+                done();
+            });
+        })
+    })
+
   })
 })
