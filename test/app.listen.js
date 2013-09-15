cedc30d23391a947f73b1fa605f461348666bb7d
Conrad Kleinespel <conradk@conradk.com>
2013-09-15 04:56:05 +0200

    Allow middleware after response has been sent.

--- test/app.listen.js
+++ test/app.listen.js
@@ -5,8 +5,9 @@ describe('app.listen()', function(){
   it('should wrap in an http.Server', function(done){
     var app = connect();
 
-    app.use(function(req, res){
+    app.use(function(req, res, next){
       res.end();
+      next();
     });
 
     app.listen(5555, function(){
