5c16ae2d07d1303f5093521da63a57607edb67ce
Douglas Christopher Wilson <doug@somethingdoug.com>
2013-10-16 14:53:31 -0400

    Update vhost example to function
    This updates the vhost example to remove all the features that have
    long ago been removed and added to express.

--- examples/vhost.js
+++ examples/vhost.js
@@ -6,7 +6,7 @@
 var connect = require('../');
 
 var account = connect(function(req, res){
-  var location = 'http://localhost:3000/account/' + req.subdomains[0];
+  var location = 'http://localhost:3000/account/' + req.headers.host.split('.localhost')[0];
   res.statusCode = 302;
   res.setHeader('Location', location);
   res.end('Moved to ' + location);
@@ -16,17 +16,12 @@ var blog = connect(function(req, res){
   res.end('blog app');
 });
 
-var main = connect(
-  connect.router(function(app){
-    app.get('/account/:user', function(req, res){
-      res.end('viewing user account for ' + req.params.user);
-    });
-
-    app.get('/', function(req, res){
-      res.end('main app');
-    });
-  })
-);
+var main = connect(function(req, res){
+  if (req.url == '/') return res.end('main app');
+  if (0 == req.url.indexOf('/account/')) return res.end('viewing user account for ' + req.url.substring(9));
+  res.statusCode = 404;
+  res.end();
+});
 
 connect(
     connect.logger()
